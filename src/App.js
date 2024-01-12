2e6b4a99c886ee0a61c3cc8296a5020bda2b493e
Joshua Franklin <franklj1@uci.edu>
2024-01-11 22:17:24 -0800

    Put the todo card in its own component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-
+import Todo from './components/Todo'
 
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <div className='card'>
-        <h2>TITLE</h2>
-        <div>
-          <button>Delete</button>
-        </div>
-      </div>
+      <Todo text='Learn React'/>
+      <Todo text='Master React'/>
+      <Todo text='Explore the full React course'/>
     </div>
   );
 }
